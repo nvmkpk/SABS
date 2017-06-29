1ba0d313b00ea7a1563708452512e6fd9605accf
raiym <r.kapishev@gmail.com>
2017-06-29 09:11:22 +0300

    Custom fragment

--- app/src/main/java/com/getadhell/androidapp/fragments/CustomBlockUrlProviderFragment.java
+++ app/src/main/java/com/getadhell/androidapp/fragments/CustomBlockUrlProviderFragment.java
@@ -3,10 +3,22 @@ package com.getadhell.androidapp.fragments;
 import android.arch.lifecycle.LifecycleFragment;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.getadhell.androidapp.R;
 import com.getadhell.androidapp.viewmodel.BlockUrlProvidersViewModel;
 
 public class CustomBlockUrlProviderFragment extends LifecycleFragment {
+
+    private EditText blockUrlProviderEditText;
+    private Button addBlockUrlProviderButton;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -14,5 +26,14 @@ public class CustomBlockUrlProviderFragment extends LifecycleFragment {
         model.getBlockUrlProviders().observe(this, blockurlProviders -> {
 
         });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_custom_url_provider, container, false);
+        blockUrlProviderEditText = (EditText) view.findViewById(R.id.blockUrlProviderEditText);
+        
+        return view;
     }
 }
