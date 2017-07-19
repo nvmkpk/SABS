76b55741f5f6b5759494ecbb1080962bc20d776f
raiym <r.kapishev@gmail.com>
2017-07-19 21:22:09 +0300

    Start with app permissions

---
+++ app/src/main/java/com/getadhell/androidapp/fragments/AppPermissionsFragment.java
@@ -0,0 +1,6 @@
+package com.getadhell.androidapp.fragments;
+
+import android.arch.lifecycle.LifecycleFragment;
+
+public class AppPermissionsFragment extends LifecycleFragment {
+}
---
+++ app/src/main/java/com/getadhell/androidapp/viewmodel/AppPermissionsViewModel.java
@@ -0,0 +1,19 @@
+package com.getadhell.androidapp.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+import com.getadhell.androidapp.db.AppDatabase;
+
+import java.util.List;
+
+public class AppPermissionsViewModel extends AndroidViewModel {
+    private AppDatabase mAppDatabase;
+    private List<String> permissions;
+
+    public AppPermissionsViewModel(Application application) {
+        super(application);
+        mAppDatabase = AppDatabase.getAppDatabase(application);
+
+    }
+}
