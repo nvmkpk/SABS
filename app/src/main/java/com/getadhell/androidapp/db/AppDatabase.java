eeaa3935129d91f71750f8025945f8368fbf9d7f
raiym <r.kapishev@gmail.com>
2017-08-13 15:07:12 +0300

    Prepare database for polocy packages

--- app/src/main/java/com/getadhell/androidapp/db/AppDatabase.java
+++ app/src/main/java/com/getadhell/androidapp/db/AppDatabase.java
@@ -21,6 +21,7 @@ import com.getadhell.androidapp.db.entity.UserBlockUrl;
 import com.getadhell.androidapp.db.entity.WhiteUrl;
 import com.getadhell.androidapp.db.migration.Migration_14_15;
 import com.getadhell.androidapp.db.migration.Migration_15_16;
+import com.getadhell.androidapp.db.migration.Migration_16_17;
 
 @Database(entities = {
         BlockUrlProvider.class,
@@ -29,10 +30,11 @@ import com.getadhell.androidapp.db.migration.Migration_15_16;
         ReportBlockedUrl.class,
         WhiteUrl.class,
         UserBlockUrl.class
-}, version = 16)
+}, version = 17)
 public abstract class AppDatabase extends RoomDatabase {
     private static final Migration MIGRATION_14_15 = new Migration_14_15(14, 15);
     private static final Migration MIGRATION_15_16 = new Migration_15_16(15, 16);
+    private static final Migration MIGRATION_16_17 = new Migration_16_17(16, 17);
     private static AppDatabase INSTANCE;
 
     public static AppDatabase getAppDatabase(Context context) {
@@ -42,6 +44,7 @@ public abstract class AppDatabase extends RoomDatabase {
                             AppDatabase.class, "adhell-database")
                             .addMigrations(MIGRATION_14_15)
                             .addMigrations(MIGRATION_15_16)
+                            .addMigrations(MIGRATION_16_17)
                             .build();
         }
         return INSTANCE;
---
+++ app/src/main/java/com/getadhell/androidapp/db/entity/PolicyPackage.java
@@ -0,0 +1,31 @@
+package com.getadhell.androidapp.db.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.arch.persistence.room.TypeConverters;
+
+import com.getadhell.androidapp.db.DateConverter;
+
+import java.util.Date;
+
+@Entity(
+        tableName = "PolicyPackage"
+)
+@TypeConverters(DateConverter.class)
+public class PolicyPackage {
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    public String id;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "createdAt")
+    public Date createdAt;
+
+    @ColumnInfo(name = "updatedAt")
+    public Date updatedAt;
+
+}
