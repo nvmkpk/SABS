f6e8edda399b94750ccdaeee325be6de3537ee87
Leefee <harukr@hotmail.com>
2017-08-30 11:40:52 -0700

    Import/Export app lists

--- app/src/main/java/com/fiendfyre/AdHell2/fragments/PackageDisablerFragment.java
+++ app/src/main/java/com/fiendfyre/AdHell2/fragments/PackageDisablerFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -35,6 +36,7 @@ import com.fiendfyre.AdHell2.db.entity.AppInfo;
 import com.fiendfyre.AdHell2.utils.AppsListDBInitializer;
 
 import java.util.List;
+import java.io.*;
 
 import javax.inject.Inject;
 
@@ -163,8 +165,113 @@ public class PackageDisablerFragment extends LifecycleFragment {
             case R.id.action_pack_dis_reload:
                 editText.setText("");
                 loadApplicationsList(true);
+                break;
+            case R.id.disabler_import_storage:
+                Toast.makeText(context, getString(R.string.imported_from_storage), Toast.LENGTH_SHORT).show();
+                importList();
+                break;
+            case R.id.disabler_export_storage:
+                Toast.makeText(context, getString(R.string.exported_to_storage), Toast.LENGTH_SHORT).show();
+                exportList();
+                break;
+            case R.id.disabler_enable_all:
+                Toast.makeText(context, getString(R.string.enabled_all_disabled), Toast.LENGTH_SHORT).show();
+                enableAllPackages();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void importList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                File file = new File(Environment.getExternalStorageDirectory(), "adhell_packages.txt");
+
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        try {
+                            AppInfo appInfo = mDb.applicationInfoDao().getByPackageName(line);
+                            appInfo.disabled = !appInfo.disabled;
+                            if (appInfo.disabled) appPolicy.setDisableApplication(line);
+                            else appPolicy.setEnableApplication(line);
+                            mDb.applicationInfoDao().insert(appInfo);
+                        }
+                        catch (Exception e) {
+                            // Ignore any potential errors
+                        }
+                    }
+                }
+                catch (IOException e) {
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void o) {
+                super.onPostExecute(o);
+                loadApplicationsList(true);
+            }
+        }.execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void exportList() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                File file = new File(Environment.getExternalStorageDirectory(), "adhell_packages.txt");
+                List<AppInfo> disabledAppList = mDb.applicationInfoDao().getDisabledApps();
+
+                try {
+                    FileOutputStream stream = new FileOutputStream(file);
+                    OutputStreamWriter writer = new OutputStreamWriter(stream);
+
+                    writer.write("");
+
+                    for (AppInfo app : disabledAppList) {
+                        writer.append(app.packageName + "\n");
+                    }
+
+                    writer.close();
+                    stream.flush();
+                    stream.close();
+                }
+                catch (IOException e) {
+                    Log.e("Exception", "File write failed: " + e.toString());
+                }
+
+                return null;
+            }
+        }.execute();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void enableAllPackages() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... o) {
+                List<AppInfo> disabledAppList = mDb.applicationInfoDao().getDisabledApps();
+
+                for (AppInfo app : disabledAppList) {
+                    app.disabled = false;
+                    appPolicy.setEnableApplication(app.packageName);
+                    mDb.applicationInfoDao().insert(app);
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void o) {
+                super.onPostExecute(o);
+                loadApplicationsList(true);
+            }
+        }.execute();
     }
 
     @SuppressLint("StaticFieldLeak")
